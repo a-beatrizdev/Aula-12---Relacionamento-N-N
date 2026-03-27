8dc4956c187beb75b4a6d8eb239b5f251384621b
Alana <alanabeatriztobias>
2026-03-27 13:51:17 -0300

    Feat: Add tabela aluno

--- main.py
+++ main.py
@@ -7,3 +7,20 @@
 # Forma simples:
 # A relação não precisa guardar dados extras
 # Só fazer o relacionamento 
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+Base = declarative_base()
+
+# Tabelas curso e aluno
+class Aluno(Base):
+    __tablename__ = "alunos"
+
+    #Como cria uma coluna
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String(100), nullable=False)
+
+    #Função para imprimir
+    def __repr__(self):
+        return f"ID: {self.id} - NOME: {self.nome}"
