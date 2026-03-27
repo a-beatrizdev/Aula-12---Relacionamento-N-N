6c2aa4659fe0e97a1614a75d61ea70b027134598
Alana <alanabeatriztobias>
2026-03-27 13:42:31 -0300

    Explicação do Relacionamento

---
+++ main.py
@@ -0,0 +1,9 @@
+# Relacionamentos Muitos para muitos (N:N)
+
+# Estudantes se inscrevem em cursos.
+# um estudante pode fazer vários cursos
+# um curso pode ter vários estudantes
+
+# Forma simples:
+# A relação não precisa guardar dados extras
+# Só fazer o relacionamento 
